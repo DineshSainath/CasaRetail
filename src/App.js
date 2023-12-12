4608841903f2e43366fa66075caca7975cee3341
dineshsainath <sainath.dinesh@gmail.com>
2023-12-12 10:34:29 -0500

    navbar done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import NavbarComponent from './NavbarComponent.jsx'
 function App() {
   return (
     <div className="App">
-      <NavbarComponent />
+      <header className="app-header">
+        <NavbarComponent />
+      </header>
       <h1>hello</h1>
     </div>
   )
--- src/NavbarComponent.jsx
+++ src/NavbarComponent.jsx
@@ -2,8 +2,13 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './NavbarComponent.css'
+import { BsSearch } from 'react-icons/bs'
+import { IoCartOutline } from 'react-icons/io5'
+import { CiHeart } from 'react-icons/ci'
+import { IoPersonOutline } from 'react-icons/io5'
 function NavbarComponent() {
   return (
     <Navbar collapseOnSelect expand="lg" className=" nav bg-body-tertiary">
@@ -36,8 +41,17 @@ function NavbarComponent() {
           </Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
+          <Nav.Link className="mx-2">
+            <BsSearch size={20} />
+          </Nav.Link>
+          <Nav.Link className="mx-2">
+            <IoCartOutline size={25} />
+          </Nav.Link>
+          <Nav.Link className="mx-2">
+            <CiHeart size={25} />
+          </Nav.Link>
+          <Nav.Link className="mx-2">
+            <IoPersonOutline size={20} />
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
