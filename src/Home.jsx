23933e55787cf790120e2480bf8ef5fc5a0fa103
dineshsainath <sainath.dinesh@gmail.com>
2023-12-12 12:17:16 -0500

    side menu done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,6 @@
 import ProfileCard from './ProfileCard'
+import MenuCard from './MenuCard'
+import './Home.css'
 
 function Home() {
   return (
@@ -6,6 +8,10 @@ function Home() {
       <div className="nav-heading">Home/Profile</div>
 
       <ProfileCard />
+      <div className="menu"></div>
+      <MenuCard name="My Profile" subName="Notifications, password" />
+      <MenuCard name="My Orders" subName="Already have 12 orders" />
+      <MenuCard name="Shipping Addresses" subName="3 addresses" />
     </div>
   )
 }
--- src/MenuCard.jsx
+++ src/MenuCard.jsx
@@ -1,3 +1,16 @@
-function MenuCard() {}
+import React from 'react'
+import './MenuCard.css'
+
+function MenuCard(props) {
+  return (
+    <div className="menu-card">
+      <div className="text-container">
+        <h4 className="menu-name">{props.name}</h4>
+        <p className="sub-name">{props.subName}</p>
+      </div>
+      <p className="arrow-link">&gt;</p>
+    </div>
+  )
+}
 
 export default MenuCard
