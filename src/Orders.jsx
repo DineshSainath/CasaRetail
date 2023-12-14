6c11dfdc821c4351ef326f23d61fa059dd38815a
dineshsainath <sainath.dinesh@gmail.com>
2023-12-14 09:08:03 -0500

    orders optimize - map

--- src/Orders.jsx
+++ src/Orders.jsx
@@ -2,41 +2,56 @@ import './Orders.css'
 import OrderCard from './OrderCard'
 
 function Orders() {
+  const orderData = [
+    {
+      orderNo: '1',
+      name: 'Madeup White Cotton Blend Checkered Slim Fit Shirt',
+      deliveryInfo: 'Express Delivery by Sat, Aug 30',
+      paymentInfo: 'Credit Card Payment',
+      price: '1,899',
+      status: 'Pending',
+    },
+    {
+      orderNo: '2',
+      name: 'Madeup White Cotton Blend Checkered Slim Fit Shirt',
+      deliveryInfo: 'Express Delivery by Sat, Aug 30',
+      paymentInfo: 'Credit Card Payment',
+      price: '1,899',
+      status: 'Pending',
+    },
+    {
+      orderNo: '3',
+      name: 'Madeup White Cotton Blend Checkered Slim Fit Shirt',
+      deliveryInfo: 'Express Delivery by Sat, Aug 30',
+      paymentInfo: 'Credit Card Payment',
+      price: '1,899',
+      status: 'Delivered',
+    },
+    {
+      orderNo: '4',
+      name: 'Madeup White Cotton Blend Checkered Slim Fit Shirt',
+      deliveryInfo: 'Express Delivery by Sat, Aug 30',
+      paymentInfo: 'Credit Card Payment',
+      price: '1,899',
+      status: 'Delivered',
+    },
+  ]
+
   return (
     <div>
       <p className="content-name">My Orders</p>
-      <OrderCard
-        orderNo="1"
-        name="Madeup White Cotton Blend Checkered Slim Fit Shirt"
-        deliveryInfo="Express Delivery by Sat, Aug 30"
-        paymentInfo="Credit Card Payment"
-        price="1,899"
-        status="Pending"
-      />
-      <OrderCard
-        orderNo="2"
-        name="Madeup White Cotton Blend Checkered Slim Fit Shirt"
-        deliveryInfo="Express Delivery by Sat, Aug 30"
-        paymentInfo="Credit Card Payment"
-        price="1,899"
-        status="Pending"
-      />
-      <OrderCard
-        orderNo="3"
-        name="Madeup White Cotton Blend Checkered Slim Fit Shirt"
-        deliveryInfo="Express Delivery by Sat, Aug 30"
-        paymentInfo="Credit Card Payment"
-        price="1,899"
-        status="Delivered"
-      />
-      <OrderCard
-        orderNo="4"
-        name="Madeup White Cotton Blend Checkered Slim Fit Shirt"
-        deliveryInfo="Express Delivery by Sat, Aug 30"
-        paymentInfo="Credit Card Payment"
-        price="1,899"
-        status="Delivered"
-      />
+
+      {orderData.map((item) => (
+        <OrderCard
+          key={item.orderNo}
+          orderNo={item.orderNo}
+          name={item.deliveryInfo}
+          deliveryInfo={item.deliveryInfo}
+          paymentInfo={item.paymentInfo}
+          price={item.price}
+          status={item.status}
+        />
+      ))}
     </div>
   )
 }
